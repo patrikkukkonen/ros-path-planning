b2dcc095f5f3e5688f874e126befb8f61efbb18e
Patrik Kukkonen <patrik.kukkonen@gmail.com>
2023-06-01 18:28:19 +0900

    create new script files to call the spawn and delete functions to reduce repetitive code

---
+++ scripts/scenario1_objects.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from object_spawner import ObjectSpawner
+import rclpy
+
+def main():
+    rclpy.init()
+    # Create an instance of the ObjectSpawner class
+    spawner = ObjectSpawner()
+    # spawn_obejcts(id, num_objects, spawn_interval, x_pose, y_pose, z_pose, horizontal=True)
+    spawner.spawn_objects(id=1, num_objects=6, spawn_interval=0.5, x_pose=-1.0, y_pose=-0.1, z_pose=0.5, horizontal=True)
+
+    #spawner.delete_objects(id=1, num_objects=6, delete_interval=0.5)
+    rclpy.shutdown()
+if __name__ == '__main__':
+    main()
---
+++ scripts/scenario2_objects.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from object_spawner import spawn_objects
+import rclpy
+
+def main():
+    rclpy.init()
+    # Create an instance of the ObjectSpawner class
+    spawner = ObjectSpawner()
+    # spawn_obejcts(id, num_objects, spawn_interval, x_pose, y_pose, z_pose, horizontal=True)
+    spawn_objects(id=1, num_objects=6, spawn_interval=0.5, x_pose=-1.0, y_pose=-0.1, z_pose=0.5, horizontal=True)
+    spawn_objects(id=2, num_objects=6, spawn_interval=0.5, x_pose=-1.0, y_pose=-2.1, z_pose=0.5, horizontal=True)
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
